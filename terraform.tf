b10a0e70f7b7a46b723126b7168e6d3eee92b3be
Craig Thacker <craigthackerx@gmail.com>
2022-11-26 22:56:50 +0000

    update gitignore

---
+++ terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  #Use the latest by default, uncomment below to pin or use hcl.lck
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      #      configuration_aliases = [azurerm.default-provider]
+      #      version = "~> 2.68.0"
+    }
+  }
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+}
---
+++ test.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "test_rg" {
+  location = "UK South"
+  name     = "test-rg"
+}
